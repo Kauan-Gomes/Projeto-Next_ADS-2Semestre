67a379004b9ca82ae718736fdd2be7e21a89071a
Kauan Gomes <kauang13@gmail.com>
2023-08-29 22:31:40 -0300

    header

---
+++ src/app/quem-somos/page.js
@@ -0,0 +1,7 @@
+
+
+export default function QuemSomos() {
+  return(
+    <h1>Bem vindo ao quem somos</h1>
+  )
+}
---
+++ src/components/layout/header.jsx
@@ -0,0 +1,17 @@
+import './Header.css'
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header id='header'>
+        <h1>logo</h1>
+        <nav>
+            <ul>
+                <li>Home </li>
+                <li>Quem Somos</li>
+                <li>Contato</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
